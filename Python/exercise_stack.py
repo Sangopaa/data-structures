da354505a04a003d598cb584bdbd50501e132f05
Sangopaa <sangopaa@gmail.com>
2023-11-01 21:16:53 -0500

    determinar si una cadena de caracteres esta balanceada o no

--- Python/exercise_stack.py
+++ Python/exercise_stack.py
@@ -2,21 +2,43 @@ from clases.stacks.stacks import Stack
 
 stack = Stack()
 
-values_expected = ['[', ']', '{', '}', '(', ')']
-aperture_values = ['[', '{', '(']
-closing_values = [']', '}', ')']
-values_verify = '[]'
-
-balanced = True
-
-for value in values_verify:
-    if value in values_expected:
-        if value in closing_values:
+values_verify = '[[]'
+    
+def isBalanced(values_verify):
+    balanced = True
+    
+    values_expected = ['[', ']', '{', '}', '(', ')']
+    aperture_values = ['[', '{', '(']
+    closing_values = [']', '}', ')']
+    
+    for value in values_verify:
+        if value in values_expected and value in closing_values:
             if stack.Size() == 0:
-               balanced = False
-        
-        if value in aperture_values:
+                balanced = False
+                break
+            
+            if stack.Peek() == aperture_values[0] and value == closing_values[0]:
+                stack.Pop()
+            
+            elif stack.Peek() == aperture_values[1] and value == [1]:
+                stack.Pop()
+            
+            elif stack.Peek() == aperture_values[2] and value == [2]:
+                stack.Pop()
+                
+            else:
+                balanced = False
+                break
+                
+        if value in values_expected and value in aperture_values:
             stack.Push(value)
-             
-    else:
-        print('no esta')
+        
+        if value not in values_expected:
+            print('Caracter no reconocido')
+            
+    if not stack.isEmpty():
+        balanced = False
+            
+    return balanced
+            
+print(isBalanced(values_verify))
